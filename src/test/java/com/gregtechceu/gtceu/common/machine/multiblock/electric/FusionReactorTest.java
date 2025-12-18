d1b3e64afcbe7be15c08ca0a0c241f9e59489c92
noname <mailzetrock1@gmail.com>
2025-12-18 20:24:58 +0300

    now the tests are working, but only on the real client (ldlib errors)

--- src/test/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/FusionReactorTest.java
+++ src/test/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/FusionReactorTest.java
@@ -1,9 +1,19 @@
 package com.gregtechceu.gtceu.common.machine.multiblock.electric;
 
 import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
+import com.gregtechceu.gtceu.api.fluids.store.FluidStorageKeys;
+import com.gregtechceu.gtceu.common.data.GTMachines;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.common.machine.storage.QuantumTankMachine;
+import com.gregtechceu.gtceu.gametest.util.TestUtils;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.gametest.GameTestHolder;
 import net.minecraftforge.gametest.PrefixGameTestTemplate;
 
@@ -11,10 +21,38 @@ import net.minecraftforge.gametest.PrefixGameTestTemplate;
 @GameTestHolder(GTCEu.MOD_ID)
 public class FusionReactorTest {
 
-    @GameTest(template = "fusionreactor", batch = "reactorTests", required = false)
+    public static void insertFluid(GameTestHelper helper, QuantumTankMachine machine, FluidStack stack) {
+        machine.getFluidHandlerCap(Direction.UP, false).fill(stack,
+                IFluidHandler.FluidAction.EXECUTE);
+    }
+
+    public static boolean isTankFluidStackEqual(QuantumTankMachine tank, FluidStack stack) {
+        return tank.getStored().getFluid() == stack.getFluid() && tank.getStoredAmount() == stack.getAmount();
+    }
+
+    @GameTest(template = "fusionreactor", batch = "fusionReactorTest", required = false)
     public static void fusionReactorTest(GameTestHelper helper) {
-        // helper.setBlock(6,3,0, GTMachines.SUPER_TANK[1].getBlock());
-        helper.runAtTickTime(40, () -> {
+        QuantumTankMachine tank1 = (QuantumTankMachine) TestUtils.setMachine(helper, new BlockPos(6, 3, 0),
+                GTMachines.SUPER_TANK[1]);
+        QuantumTankMachine tank2 = (QuantumTankMachine) TestUtils.setMachine(helper, new BlockPos(8, 3, 0),
+                GTMachines.SUPER_TANK[1]);
+        QuantumTankMachine tank3 = (QuantumTankMachine) TestUtils.setMachine(helper, new BlockPos(9, 2, 0),
+                GTMachines.SUPER_TANK[1]);
+        FusionReactorMachine machine = (FusionReactorMachine) ((MetaMachineBlockEntity) helper
+                .getBlockEntity(new BlockPos(7, 2, 0)))
+                .getMetaMachine();
+
+        insertFluid(helper, tank1, GTMaterials.Deuterium.getFluid(125));
+        insertFluid(helper, tank2, GTMaterials.Tritium.getFluid(125));
+        machine.setWorkingEnabled(true);
+
+        helper.runAtTickTime(5, () -> {
+            helper.assertTrue(machine.isFormed(), "fusion reactor didn't formed");
+        });
+
+        helper.runAtTickTime(20, () -> {
+            helper.assertTrue(isTankFluidStackEqual(tank3, GTMaterials.Helium.getFluid(FluidStorageKeys.PLASMA, 125)),
+                    "fusion reactor didn't generate Helium plasma ");
             helper.succeed();
         });
     }
